c44d64b1f0a7fa52cdb17e85e569ea14e9538f2b
river20s <river20s@naver.com>
2025-07-20 23:07:20 +0900

    EOF 실제 값 확인하기 - EOF 값을 확인하기 위한 line 7 추가 - 확인 결과 -1

--- Project250720/hello.c
+++ Project250720/hello.c
@@ -4,6 +4,7 @@
 main()
 {
 	int c;
+	printf("EOF is %d\n", EOF); // This will print the value of EOF, which is typically -1
 	
 	while (c = getchar() != EOF) // Note: This line has a bug, it should be `(c = getchar()) != EOF` 
 	{
